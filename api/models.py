81a96c94714d3a8719b21c7864305c08eebd1a70
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-20 22:00:32 +0600

    feat: created profile model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    phone = models.CharField(null=False, blank=False, max_length=254)
+    photo = models.URLField(max_length=254, null=True, blank=True)
+    verification = models.CharField(max_length=6)
+
+    class Meta:
+        verbose_name = 'profile'
+        verbose_name_plural = 'profiles'
+    def __str__(self):
+        return self.phone
