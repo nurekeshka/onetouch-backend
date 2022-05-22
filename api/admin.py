c73901a15074634dff163f8e1473d15dba52aa98
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-22 18:15:09 +0600

    feat: better admin display

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone', 'verification', 'photo')
+    fields = ('phone', 'verification', 'photo')
