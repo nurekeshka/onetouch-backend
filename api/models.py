67a736c2181ade7f2b21d9e0c3e099ba99b76217
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-22 17:07:08 +0600

    feat: changed verification max length

--- api/models.py
+++ api/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     phone = models.CharField(null=False, blank=False, max_length=254)
     photo = models.URLField(max_length=254, null=True, blank=True)
-    verification = models.CharField(max_length=6)
+    verification = models.CharField(max_length=10)
 
     class Meta:
         verbose_name = 'profile'
