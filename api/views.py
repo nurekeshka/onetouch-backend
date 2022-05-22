f843de3106d7e050101eeefb03d11a575c629af4
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-22 18:31:20 +0600

    fix: minor bugs

--- api/views.py
+++ api/views.py
@@ -6,29 +6,31 @@ from django.conf import settings
 import requests, urllib.parse
 from random import randint
 from .models import Profile
+from .serializers import UserSerializer, ProfileSerializer
 
 
-COMPLETED = settings.COMPLETED
-API_KEY = settings.API_KEY
+@api_view(['GET'])
+def test(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['POST'])
 def send_profile_verification(request):
     ''' Отправка СМС сообщения для верификации телефонного номера '''
-    phone = _format_phone_number(request.POST.get('phone'))
+    ''' profiles/send-sms-verification '''
+    phone = request.POST.get('phone')
     code = randint(1000, 9999)
     
-    try:
-        profile = Profile.objects.get(phone=phone)
+    if Profile.objects.filter(phone=phone).exists():
         return Response(data='Profile already exists', status=406)
-    except Profile.DoesNotExist:
-        pass
 
     # response = requests.get(
     #     url='https://api.mobizon.kz/service/Message/SendSmsMessage',
     #     params={
-    #         'apiKey': API_KEY,
-    #         'recipient': urllib.parse.quote(phone),
+    #         'apiKey': settings.API_KEY,
+    #         'recipient': urllib.parse.quote(_format_phone_number(request.POST.get('phone'))),
     #         'text': urllib.parse.quote(f'Код для верификации номера: {code}')
     #     }
     # )
@@ -37,35 +39,27 @@ def send_profile_verification(request):
     profile = Profile.objects.create(phone=phone, verification=code)
     profile.save()
 
+    serializer = ProfileSerializer(profile, many=False)
+
     # return Response(response.json(), status=response.status_code)
-    return Response(data='Created')
-
-
-@api_view(['GET'])
-def profile_is_verified(request):
-    ''' Проверка завершена-ли верификация '''
-    try:
-        profile = Profile.objects.get(phone=request.GET.get('phone'))
-    except Profile.DoesNotExist:
-        return Response(data='Profile with that phone number does not exist', status=406)
-
-    return Response(_profile_is_verified(profile))
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def verify_profile_phone(request):
     ''' Верификация телефонного номера с проверкой СМС кода '''
+    ''' profiles/verify-phone '''
     phone = request.GET.get('phone')
     code = request.GET.get('code')
 
-    try:
-        profile = Profile.objects.get(phone=phone)
-    except Profile.DoesNotExist:
+    if not Profile.objects.filter(phone=phone).exists():
         return Response(data='Profile with that phone number does not exist', status=406)
 
-    if profile.verification != COMPLETED:
+    profile = Profile.objects.get(phone=phone)
+
+    if profile.verification != settings.COMPLETED:
         if profile.verification == code:
-            profile.verification = COMPLETED
+            profile.verification = settings.COMPLETED
             profile.save()
             return Response(data='Success', status=200)
         else:
@@ -74,31 +68,43 @@ def verify_profile_phone(request):
         return Response(data='Profile is verified', status=400)
 
 
+@api_view(['GET'])
+def profile_is_verified(request):
+    ''' Проверка завершена-ли верификация '''
+    ''' profiles/verified '''
+    profile = Profile.objects.filter(phone=request.GET.get('phone'))
+
+    if profile.exists():
+        return Response(profile[0].is_verified())
+    else:
+        return Response(data='Profile with that phone number does not exist', status=406)    
+
+
 @api_view(['POST'])
 def create_verified_user(request):
     ''' После проверки пользователя заполнение информации '''
     phone = request.POST.get('phone')
     
-    try:
-        profile = Profile.objects.get(phone=phone)
-    except Profile.DoesNotExist:
+    if not Profile.objects.filter(phone=phone).exists():
         return Response(data='Profile with that number does not exist', status=406)
 
-    if profile.verification == COMPLETED:
+    profile = Profile.objects.get(phone=phone)
+
+    if profile.verification == settings.COMPLETED:
         if not profile.user:
             user = User.objects.create(
                 username=request.POST.get('username'),
-                password=request.POST.get('password'),
                 first_name=request.POST.get('first_name'),
                 last_name=request.POST.get('last_name')
             )
 
+            user.set_password(request.POST.get('password'))
             user.save()
 
             profile.user = user
             profile.photo = request.POST.get('photo')
-
             profile.save()
+
             return Response(data='User created successfully', status=201)
         else:
             return Response(data='User with that profile is already created', status=406)
@@ -110,7 +116,7 @@ def create_verified_user(request):
 def sign_in(request):
     user = authenticate(
         request=request,
-        username=request.GET.get('username'),
+        username=request.POST.get('username'),
         password=request.POST.get('password')
     )
 
@@ -130,12 +136,6 @@ def sign_out(request):
 def is_authenticated(request):
     return Response(request.user.is_authenticated)
 
-
-def _profile_is_verified(profile: Profile) -> bool:
-    if profile.verification == COMPLETED:
-        return True
-    else:
-        return False
 
 def _format_phone_number(phone: str) -> str:
     if ~phone.find('+'):
