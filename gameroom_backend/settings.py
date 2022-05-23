210fcbb89f988f9f6331777fa5c0373d38a3591f
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-23 08:11:17 +0600

    feat: add authtoken module

--- gameroom_backend/settings.py
+++ gameroom_backend/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     'api.apps.ApiConfig'
 ]
