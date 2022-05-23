75dd9661c6e6ed99b098422fcc51d9c08c721717
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-23 08:15:18 +0600

    feat: add default authentication class - token

--- gameroom_backend/settings.py
+++ gameroom_backend/settings.py
@@ -82,6 +82,14 @@ DATABASES = {
     }
 }
 
+# REST_FRAMEWORK Authentication
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
