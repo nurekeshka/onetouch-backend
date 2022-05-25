d6f954212a71cca5ac0749d46ba535f6e92300c1
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-25 11:32:15 +0600

    init: renamed main folder

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Profile
 
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'phone', 'verification', 'photo')
-    fields = ('phone', 'verification', 'photo')
+# Register your models here.
--- api/models.py
+++ api/models.py
@@ -1,20 +1,3 @@
-from django.contrib.auth.models import User
 from django.db import models
-from django.conf import settings
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    phone = models.CharField(null=False, blank=False, max_length=254)
-    photo = models.URLField(max_length=254, null=True, blank=True)
-    verification = models.CharField(max_length=10)
-
-    class Meta:
-        verbose_name = 'profile'
-        verbose_name_plural = 'profiles'
-    def __str__(self):
-        return self.user.username
-    def is_verified(self):
-        if self.verification == settings.COMPLETED:
-            return True
-        else:
-            return False
+# Create your models here.
