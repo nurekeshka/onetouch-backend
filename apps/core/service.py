e8a6346052b463ab4a3c62d3c88f5d12da08027a
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-26 15:16:24 +0600

    feat: start_new_verification

---
+++ apps/core/service.py
@@ -0,0 +1,43 @@
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+from .models import User, Verification
+from .constants import send_sms_url
+from random import randint
+from requests import get
+
+
+def start_new_verification(payload: str):
+    if User.objects.filter(phone=payload).exists():
+        return {
+            'error': 'user with that phone number already exists',
+            'success': False
+            }, 409
+
+    if Verification.objects.filter(phone=payload).exists():
+        Verification.objects.get(phone=payload).delete()
+    
+    verification = Verification.objects.create(phone=payload, code=randint(1000, 9999))
+
+    response = get(
+        url=send_sms_url,
+        params={
+            'apiKey': settings.API_KEY,
+            'recipient': verification.phone,
+            'text': f'GameRoom account verification code: {verification.code}'
+        }
+    )
+
+    data = response.json()
+
+    if data.get('code') is 0:
+        verification.save()
+        return {
+            'success': True,
+            'verification': {
+                'phone': verification.phone,
+                'code': verification.code
+            }
+        }, 201
+    else:
+        verification.delete()
+        return data, response.status_code
