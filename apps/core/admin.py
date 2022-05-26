6cddbeb64bbf14b23693b21e55563cdf2a33ed0f
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-26 15:13:37 +0600

    feat: register user and verification

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name','phone')
+    fields = (
+        ('phone', 'password'),
+        ('first_name', 'last_name'),
+        ('username', 'photo'),
+        ('email', 'birth_date'),
+        'groups', 'user_permissions',
+    )
+
+@admin.register(models.Verification)
+class VerificationAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'code')
+    fields = list_display
