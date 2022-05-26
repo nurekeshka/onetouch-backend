1a10dccf265df8f9cb6ada42e25b86df94958420
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-26 15:14:34 +0600

    feat: add sms verification

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from . import service
 
-# Create your views here.
+
+@api_view(['GET'])
+def verification_sms(request):
+    phone = request.GET.get('phone')
+
+    if phone is None:
+        return Response(data={'error': 'phone is not specified'}, status=400)
+    
+    body, code = service.start_new_verification(phone)
+
+    return Response(data=body, status=code)
