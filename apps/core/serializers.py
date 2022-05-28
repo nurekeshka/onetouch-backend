12220204c56930e14ca046068056d35859fc7e2e
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-29 02:20:47 +0600

    feat: verification serializer

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -5,7 +5,7 @@ import phonenumbers as pns
 from .models import User, Verification
 
 
-class CreateUserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'photo', 'phone', 'birth_date')   
@@ -16,13 +16,6 @@ class CreateUserSerializer(serializers.ModelSerializer):
         return user
 
     def validate_phone(self, value):
-        ''' Check if phone answers the format '''
-        try:
-            if pns.is_possible_number(pns.parse(value)):
-                pass
-        except:
-            raise serializers.ValidationError('Phone has wrong format. It should contain: Country Code, National Number')
-
         ''' Check if phone exists in verification table '''
         if not Verification.objects.filter(phone=value).exists():
             raise serializers.ValidationError('Phone does not exist in verification table')
@@ -34,19 +27,32 @@ class CreateUserSerializer(serializers.ModelSerializer):
         return value
 
 
-class GetUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'photo', 'phone', 'birth_date', 'date_joined')
-    def validate_phone(self, value):
-        try:
-            if pns.is_possible_number(pns.parse(value)):
-                return value
-        except:
-            raise serializers.ValidationError('Phone has wrong format. It should contain: Country Code, National Number')
-
-
 class TokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Token
         fields = ('key',)
+
+
+class VerificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Verification
+        fields = ('phone', 'code')
+
+    def create(self, validated_data):
+        phone = validated_data.get('phone')
+
+        if Verification.objects.filter(phone=phone).exists():
+            Verification.objects.get(phone=phone).delete()
+
+        return Verification.objects.create(**validated_data)
+
+    def validate_phone(self, value):
+        ''' Check if phone answers the format '''
+        try:
+            if pns.is_possible_number(pns.parse(value)):
+                pass
+        except:
+            raise serializers.ValidationError('Phone has wrong format. It should contain: Country Code, National Number')
+        
+        if User.objects.filter(phone=value).exists():
+            raise serializers.ValidationError('User with that phone number already exists')
