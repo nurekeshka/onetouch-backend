739165a1327abd82cb7d0776a55002d29b6a16f4
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-28 22:13:08 +0600

    feat: add commented csrf_exempt

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,4 @@
+# from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
