10cb555c026f062943903b5836ef3b60dc8728b2
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-29 00:55:52 +0600

    feat: add create function

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+import phonenumbers as pns
 from .models import User
 
 
@@ -7,6 +8,11 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'photo', 'phone', 'birth_date')
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data.get('password'))
+        return user
+
 
 class TokenSerializer(serializers.ModelSerializer):
     class Meta:
