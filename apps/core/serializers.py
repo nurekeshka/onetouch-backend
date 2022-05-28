c62456917a6659a0f7c470f2eed07f9463a0fb5f
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-29 01:15:44 +0600

    feat: add phone format validation

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -8,6 +8,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'photo', 'phone', 'birth_date')
+    def validate_phone(self, value):
+        try:
+            if pns.is_possible_number(pns.parse(value)):
+                pass
+        except:
+            raise serializers.ValidationError('Phone has wrong format. It should contain: Country Code, National Number')
+        
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
         user.set_password(validated_data.get('password'))
