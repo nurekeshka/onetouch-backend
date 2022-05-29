9079dce3b5b304ea27a01f4dbfd42f1fbb70c3b9
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-29 17:41:34 +0600

    feat: register Photo model

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Photo)
+class Photo(admin.ModelAdmin):
+    list_display = ('id', 'link')
+    fields = ('link',)
