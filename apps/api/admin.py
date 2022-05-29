1095f230078c805f2321749a06e67d006cdbee64
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-29 20:12:31 +0600

    feat: registered feedback, photo, field, game models

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -3,6 +3,20 @@ from . import models
 
 
 @admin.register(models.Photo)
-class Photo(admin.ModelAdmin):
+class PhotoAdmin(admin.ModelAdmin):
     list_display = ('id', 'link')
     fields = ('link',)
+
+@admin.register(models.Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('raiting', 'description', 'user', 'field')
+
+@admin.register(models.Field)
+class FieldAdmin(admin.ModelAdmin):
+    list_display = ('address', 'photo', 'contacts')
+    fields = ('address', 'photo', 'contacts')
+
+@admin.register(models.Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('field', 'form', 'date', 'start', 'end')
+    fields = ('field', 'form', 'date', 'start', 'end', 'signed_users')
