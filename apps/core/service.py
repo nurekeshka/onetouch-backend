f6d95c742b041cff4b7148a0f526d37e7a799c64
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-29 16:36:06 +0600

    feat: validation using serializer

--- apps/core/service.py
+++ apps/core/service.py
@@ -1,30 +1,22 @@
-from .serializers import GetUserSerializer, CreateUserSerializer, TokenSerializer
+from .serializers import UserSerializer, TokenSerializer, VerificationSerializer
 from rest_framework.authtoken.models import Token
 from django.conf import settings
-from .models import User, Verification
+from .models import Verification
 from .constants import send_sms_url, confirmed
-import phonenumbers as pns
-from random import randint
 from requests import get
+from random import randint
 
 
 def start_new_verification(payload: str) -> tuple:
-    if not is_valid_phone(payload):
-        return {
-                'error': 'invalid phone number',
-                'success': False,
-            }, 400
-
-    if User.objects.filter(phone=payload).exists():
-        return {
-            'error': 'user with that phone number already exists',
-            'success': False
-            }, 409
-
     if Verification.objects.filter(phone=payload).exists():
         Verification.objects.get(phone=payload).delete()
+
+    serializer = VerificationSerializer(data={'phone': payload, 'code': randint(1000, 9999)}, many=False)
     
-    verification = Verification.objects.create(phone=payload, code=randint(1000, 9999))
+    if serializer.is_valid():
+        verification = serializer.create(serializer.validated_data)
+    else:
+        return serializer.errors, 400
 
     response = get(
         url=send_sms_url,
@@ -39,51 +31,28 @@ def start_new_verification(payload: str) -> tuple:
 
     # if data.get('code') is 0:
     verification.save()
-    return {
-        'success': True,
-        'verification': {
-            'phone': verification.phone,
-            'code': verification.code
-        }
-    }, 201
+    return serializer.data, 201
     # else:
     #     verification.delete()
     #     return data, response.status_code
 
 
-def verification(number: str, code: str) -> tuple:
-    if not is_valid_phone(number):
-        return {
-                'error': 'invalid phone number',
-                'success': False,
-            }, 400
-
-    if not Verification.objects.filter(phone=number, code=code).exists():
-        return {
-            'error': 'does not seem that phone and code match together',
-            'success': False
-        }, 404
+def verification(payload: dict) -> tuple:
+    try:
+        verification = Verification.objects.get(phone=payload.get('phone'))
+    except Verification.DoesNotExist:
+        return {'phone': ['Does not exist in verification table']}, 404
     
-    verification = Verification.objects.get(phone=number, code=code)
+    if verification.code == confirmed: return {'phone': ['Already verified']}, 400
+
     verification.code = confirmed
     verification.save()
 
-    return {
-        'success': True,
-        'message': 'verification went successfully'
-    }, 200
-
-
-def is_valid_phone(payload: str) -> bool:
-    try:
-        if pns.is_possible_number(pns.parse(payload)):
-            return True
-    except:
-        return False
+    return {'phone': ['Successfully verified']}, 200
 
 
 def create_verified_user(info: dict) -> tuple:
-    user_serializer = CreateUserSerializer(data=info, many=False)
+    user_serializer = UserSerializer(data=info, many=False)
     
     if user_serializer.is_valid():
         user = user_serializer.create(user_serializer.validated_data)
@@ -91,7 +60,7 @@ def create_verified_user(info: dict) -> tuple:
     else:
         return user_serializer.errors, 400
 
-    # verification.delete()
+    verification.delete()
 
     token = Token.objects.get(user=user)
     token_serializer = TokenSerializer(token, many=False)
