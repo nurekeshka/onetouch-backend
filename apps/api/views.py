09327c2629c38214cfaa961ebe3379f0ae85cf42
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-29 21:56:53 +0600

    feat: get games func

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from . import service
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_all_games(request):
+    body, status = service.get_all_games(request.GET)
+    return Response(data=body, status=status)
