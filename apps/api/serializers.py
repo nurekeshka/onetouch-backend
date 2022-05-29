1c4127d8bd0b1bd54d9404d3d14baf22f4e9720d
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-29 22:24:51 +0600

    feat: field, feedback

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from .models import Game
+from .models import Game, Field, Feedback
 
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = ('id', 'field', 'form', 'date', 'start', 'end', 'signed_users')
+
+
+class FieldSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Field
+        fields = ('id', 'address', 'photo', 'contacts')
+
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feedback
+        fields = ('id', 'raiting', 'description', 'user', 'field')
