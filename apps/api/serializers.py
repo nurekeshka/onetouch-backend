596b7cf524f74aebc6ae95e7cd5733ba1262dbb7
nurekeshka <crystalmaiden666123@gmail.com>
2022-05-31 19:43:26 +0600

    feat: add game feed serializer

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -8,10 +8,19 @@ class GameSerializer(serializers.ModelSerializer):
         fields = ('id', 'field', 'form', 'date', 'start', 'end', 'signed_users')
 
 
+class GameFeedSerializer(serializers.Serializer):
+    address = serializers.CharField()
+    field_raiting = serializers.FloatField()
+    latitude = serializers.FloatField()
+    longitude = serializers.FloatField()
+    players_left = serializers.IntegerField()
+    photo = serializers.URLField()
+
+
 class FieldSerializer(serializers.ModelSerializer):
     class Meta:
         model = Field
-        fields = ('id', 'address', 'photo', 'contacts')
+        fields = ('id', 'address', 'latitude', 'longitude', 'photo', 'contacts')
 
 
 class FeedbackSerializer(serializers.ModelSerializer):
