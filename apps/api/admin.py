dbf9c502af8470d2aac5d8822b11cda896adb60c
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-02 00:20:32 +0600

    feat: add facility field to Field model

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -14,7 +14,7 @@ class FeedbackAdmin(admin.ModelAdmin):
 @admin.register(models.Field)
 class FieldAdmin(admin.ModelAdmin):
     list_display = ('address', 'latitude', 'longitude', 'photo', 'contacts')
-    fields = ('address','latitude', 'longitude', 'photo', 'contacts')
+    fields = ('address','latitude', 'longitude', 'photo', 'contacts', 'facilities')
 
 @admin.register(models.Game)
 class GameAdmin(admin.ModelAdmin):
