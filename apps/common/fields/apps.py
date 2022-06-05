3b5aa4fa586787b20efba6f8f4ed1345f1da94b9
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-06 02:54:29 +0600

    fix: common fields

--- apps/common/fields/apps.py
+++ apps/common/fields/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FieldsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.common.field'
+    name = 'apps.common.fields'
