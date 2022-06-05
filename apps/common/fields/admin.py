03a12b88894f9ca35d2c0ca63168f446ce289107
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-06 02:20:22 +0600

    feat: add field, photo, feedback, facility

---
+++ apps/common/fields/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'link')
+    fields = ('link',)
+
+@admin.register(models.Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('raiting', 'description', 'user', 'field')
+
+@admin.register(models.Field)
+class FieldAdmin(admin.ModelAdmin):
+    list_display = ('address', 'latitude', 'longitude', 'contacts')
+    fields = ('address','latitude', 'longitude', 'photo', 'contacts', 'facilities')
+
+@admin.register(models.Facility)
+class FacilityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'icons_link')
+    fields = ('name', 'icons_link')
+    order_by = ('name')
