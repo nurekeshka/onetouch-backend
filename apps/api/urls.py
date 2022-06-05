930c311843fcd82c116882e4abd36d2401881c0c
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-06 02:42:33 +0600

    feat: add fields urls

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('auth/', include('apps.accounts.urls')),
     path('games/', include('apps.common.games.urls')),
-    path('test/', views.test),
-    path('fake-information/', views.create_fake_info),
+    path('fields/', include('apps.common.fields.urls'))
 ]
