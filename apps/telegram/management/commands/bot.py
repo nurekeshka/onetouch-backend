a93633bd6a4c09ea32f6541cb16cdde3370bdbe1
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-07 00:20:30 +0600

    feat: add telegram bot setup command

---
+++ apps/telegram/management/commands/bot.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+
+
+class TelegramBotSetupCommand(BaseCommand):
+    help = 'Telegram bot setup command '
+
+    def handle(self, *args, **kwargs):
+        pass
