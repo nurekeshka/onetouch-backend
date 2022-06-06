514efecc3b0ca73ba9bb8abfc6aba7940848d874
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-07 00:13:16 +0600

    fix: renamed all service.py to utils.py

--- apps/common/fields/views.py
+++ apps/common/fields/views.py
@@ -1,17 +1,17 @@
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from . import service
+from . import utils
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def create_fake_information(request):
     if request.user.is_staff:
-        service.create_fake_information()
+        utils.create_fake_information()
 
 
 @api_view(['GET'])
 def get_latitude_and_longitude(request):
-    response = service.get_lat_and_long(request.GET.get('address'))
+    response = utils.get_lat_and_long(request.GET.get('address'))
     return Response(data=response)
--- apps/common/games/views.py
+++ apps/common/games/views.py
@@ -1,13 +1,13 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from . import service
+from . import utils
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_all(request):
-    body, status = service.get_all_for_day(
+    body, status = utils.get_all_for_day(
         date=request.GET.get('date'),
         ordering=request.GET.get('ordering')
     )
