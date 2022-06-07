f7c6a995f80d689f0ebb4cac48bd31b2925f8e5d
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-07 17:24:07 +0600

    feat: add start route

--- apps/telegram/management/commands/bot.py
+++ apps/telegram/management/commands/bot.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from ... import constants as text
+from ... import models
 from django.conf import settings
 import telebot
 
@@ -8,7 +9,16 @@ bot = telebot.TeleBot(settings.TELEGRAM_BOT_API_KEY, threaded=False)
 
 
 @bot.message_handler(['start'])
-def echo(message):
+def start(message):
+    models.Telegram.objects.get_or_create(
+        id=message.from_user.id,
+        defaults={
+            'username': message.from_user.username,
+            'first_name': message.from_user.first_name,
+            'last_name': message.from_user.last_name
+        }
+    )
+
     bot.send_message(message.chat.id, text.START_MESSAGE)
 
 
