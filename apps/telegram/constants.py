48a39454cb4172ae84047e1745ff0cfe27754a35
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-08 03:44:17 +0600

    feat: games

--- apps/telegram/constants.py
+++ apps/telegram/constants.py
@@ -2,22 +2,26 @@
 
 class Commands:
     start = 'start'
-    profile = 'profile'
     test = 'test'
+    update = 'update'
+    profile = 'profile'
+    games = 'games'
 
 
 class Messages:
     start = 'Добро пожаловать, футболист!\nЗдесь ты можешь зарегистрироваться на игру или посмотреть историю своих матчей'
     test = 'Test'
+    update = 'Введите эту информацию'
+    profile = 'Вот ваш профиль:'
+    games = 'Список всех активных игр:'
 
 
 class ButtonTexts:
+    start = 'Start'
     test = 'Test Text'
-    profile = 'Заполнить профиль'
-
-
-class Callbacks:
-    profile = 'profile'
+    update = 'Заполнить профиль'
+    profile = 'Профиль'
+    games = 'Игры'
 
 
 class Emoji:
