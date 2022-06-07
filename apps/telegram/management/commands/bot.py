71a713561f4dde40e9fba752624171fad6910a1f
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-08 04:01:09 +0600

    feat: start, telegram user decorator

--- apps/telegram/management/commands/bot.py
+++ apps/telegram/management/commands/bot.py
@@ -6,33 +6,49 @@ from telebot.types import InlineKeyboardButton
 from telebot import TeleBot
 from django.conf import settings
 from ... import constants as const
-from ... import models
+from ...models import Telegram
 
 
 bot = TeleBot(settings.TELEGRAM_BOT_API_KEY, threaded=False)
 
 
-@bot.message_handler([const.Commands.start])
-def start(message):
-    user, created  = models.Telegram.objects.get_or_create(
-        id=message.from_user.id,
-        defaults={
-            'username': message.from_user.username,
-            'first_name': message.from_user.first_name,
-            'last_name': message.from_user.last_name
-        }
-    )
+def telegram_user(function):
+    def inner(message):
+        user, created  = Telegram.objects.get_or_create(
+            id=message.from_user.id,
+            defaults={
+                'username': message.from_user.username,
+                'first_name': message.from_user.first_name,
+                'last_name': message.from_user.last_name
+            }
+        )
+        function(message, user, created)
+    return inner
 
+
+@bot.message_handler([const.Commands.start])
+@telegram_user
+def start(message, user, created):
     markup = InlineKeyboardMarkup()
 
-    # if created:
-    fill_profile = InlineKeyboardButton(
-        text=const.ButtonTexts.profile,
-        callback_data=const.Commands.profile
-    )
-    markup.add(fill_profile)
-    # else:
-    #     pass
+    if created or not user.is_active():
+        markup.add(
+            InlineKeyboardButton(
+                text=const.ButtonTexts.update,
+                callback_data=const.Commands.update
+            )
+        )
+    else:
+        markup.add(
+            InlineKeyboardButton(
+                text=const.ButtonTexts.profile,
+                callback_data=const.Commands.profile
+            ),
+            InlineKeyboardButton(
+                text=const.ButtonTexts.games,
+                callback_data=const.Commands.games
+            )
+        )
 
     bot.send_message(
         chat_id=message.chat.id, 
@@ -45,25 +61,22 @@ def start(message):
 @bot.callback_query_handler(func=lambda call: True)
 def query_callback(call):
     match call.data:
-        case const.Callbacks.profile:
-            profile()
+        case const.Commands.update:
+            user = Telegram.objects.get_or_create
+
+            bot.send_message(
+                chat_id=call.message.chat.id,
+                text='Введите свой возраст'
+            )
 
 
-@bot.message_handler([const.Commands.test])
-def test(message):
-    bot.send_message(
-        chat_id=message.chat.id,
-        text=const.Messages.test,
-        parse_mode='html'
-    )
-
-
-@bot.message_handler([const.Commands.profile])
-def profile(message):
-    bot.send_message(
-        chat_id=message.chat.id,
-        text='Profile'
-    )
+# @bot.message_handler([const.Commands.test])
+# def test(message):
+#     bot.send_message(
+#         chat_id=message.chat.id,
+#         text=const.Messages.test,
+#         parse_mode='html'
+#     )
 
 
 class Command(BaseCommand):
