88d2deb38298e15cb9bd2d0cc0533c3cc1f3665a
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-08 02:52:13 +0600

    feat: commands, messages, buttontexts, emoji

--- apps/telegram/constants.py
+++ apps/telegram/constants.py
@@ -1,1 +1,26 @@
-START_MESSAGE = 'Start message'
+
+
+class Commands:
+    start = 'start'
+    profile = 'profile'
+
+
+class Messages:
+    start = 'Добро пожаловать, футболист!\nЗдесь ты можешь зарегистрироваться на игру или посмотреть историю своих матчей'
+    test = 'Test'
+
+
+class ButtonTexts:
+    profile = 'Заполнить профиль'
+
+
+class Emoji:
+    football = '⚽'
+    heart = '❤️'
+    explosion = '💥'
+    bomb = '💣'
+    rocket = '🚀'
+    fire = '🔥'
+    commet = '☄️'
+    lightning = '⚡'
+    danger = '⚠️'
