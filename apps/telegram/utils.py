1630eec5ca0f85866a302589c4d19473cb1fcc83
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-08 18:13:52 +0600

    feat: telegram_user decorator

---
+++ apps/telegram/utils.py
@@ -0,0 +1,16 @@
+from .models import Telegram
+import telebot
+
+
+def telegram_user(function):
+    def inner(payload: telebot.types.Message):
+        user, created  = Telegram.objects.get_or_create(
+            id=payload.from_user.id,
+            defaults={
+                'username': payload.from_user.username,
+                'first_name': payload.from_user.first_name,
+                'last_name': payload.from_user.last_name
+            }
+        )
+        function(payload, user)
+    return inner
