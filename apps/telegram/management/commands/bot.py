1e610ce189745d98bf1ef40927c7b88f9d187d2a
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-08 18:39:52 +0600

    fix: update markup is one time keyboard

--- apps/telegram/management/commands/bot.py
+++ apps/telegram/management/commands/bot.py
@@ -51,7 +51,8 @@ def start(message, user):
 def query_callback(call: telebot.types.CallbackQuery, user: Telegram):
     match call.data:
         case const.Commands.update:
-            keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+            keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1, one_time_keyboard=True)
+
             keyboard.add(
                 KeyboardButton(text='Возраст'),
                 KeyboardButton(text='Номер телефона')
