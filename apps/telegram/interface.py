a8f42695d589937d6727f1fd6f51e2fdfeff799e
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-10 03:15:43 +0600

    feat: profile and menu

---
+++ apps/telegram/interface.py
@@ -0,0 +1,74 @@
+from .models import Telegram
+from .constants import Emoji
+from telebot import types
+
+
+class Start:
+    name = 'start'
+    message = 'Добро пожаловать, футболист!\nЗдесь ты можешь зарегистрироваться на игру или посмотреть историю своих матчей'
+
+
+class Back:
+    text = '« Вернуться назад'
+
+
+class Menu:
+    name = 'menu'
+    message = 'Вот меню. Что хотите сделать?'
+
+    def markup(user: Telegram):
+        inline = types.InlineKeyboardMarkup()
+
+        if user.is_active():
+            inline.add( Profile.button )
+        else:
+            inline.add( Profile.button )
+        
+        return inline
+
+
+class Profile:
+    name = 'profile'
+    text = 'Профиль'
+    button = types.InlineKeyboardButton(
+        text=text,
+        callback_data=name
+    )
+
+    def message(user: Telegram):
+        text = Emoji.fire.value + _bold(' Вот ваш невероятный профиль ') + Emoji.fire.value + '\n\n'
+
+        for key, value in user.info().items():
+            if value is None:
+                value = Emoji.stop.value
+            text += f'{_bold(key.title())}: {value}\n'
+        
+        return text
+
+    def markup(user: Telegram):
+        inline = types.InlineKeyboardMarkup(keyboard=None, row_width=2)
+        buttons = list()
+
+        # Edit buttons
+        for key, value in user.edit().items():
+            buttons.append(
+                types.InlineKeyboardButton(
+                    text=f'Изменить {key}',
+                    callback_data=value
+                )
+            )
+        
+        inline.add(*buttons)
+        
+        # Back button
+        inline.add( types.InlineKeyboardButton(
+                text=Back.text,
+                callback_data=Menu.name
+            ) 
+        )
+        
+        return inline
+
+
+def _bold(text: str) -> str:
+    return '<b>' + str(text) + '</b>'
--- apps/telegram/management/commands/bot.py
+++ apps/telegram/management/commands/bot.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from ...models import Telegram
 from ...utils import telegram_user
-from ...constants import *
+from ...interface import *
 from telebot import types
 from telebot import TeleBot
 
@@ -13,19 +13,54 @@ bot = TeleBot(settings.TELEGRAM_BOT_API_KEY, threaded=False)
 @bot.message_handler(commands=[Start.name])
 @telegram_user
 def start(message: types.Message, user: Telegram):
-    inline = types.InlineKeyboardMarkup()
-
-    if user.is_active():
-        inline.add( Profile.button )
-    else:
-        inline.add()
-    
     bot.send_message(
         chat_id=message.chat.id, 
         text=Start.message,
-        reply_markup=inline,
+        reply_markup=Menu.markup(user),
         parse_mode='html'
     )
+
+
+@bot.message_handler(commands=[Profile.name])
+@telegram_user
+def profile(message: types.Message, user: Telegram):
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=Profile.message(user),
+        reply_markup=Profile.markup(user),
+        parse_mode='html'
+    )
+
+
+@bot.callback_query_handler(func=lambda call: True)
+@telegram_user
+def callback_handler(call: types.CallbackQuery, user: Telegram):
+    match call.data:
+        case Menu.name:
+            bot.edit_message_text(
+                chat_id=call.message.chat.id,
+                message_id=call.message.id,
+                text=Menu.message,
+                parse_mode='html'
+            )
+            bot.edit_message_reply_markup(
+                chat_id=call.message.chat.id,
+                message_id=call.message.id,
+                reply_markup=Menu.markup(user)
+            )
+
+        case Profile.name:
+            bot.edit_message_text(
+                chat_id=call.message.chat.id,
+                message_id=call.message.id,
+                text=Profile.message(user),
+                parse_mode='html'
+            )
+            bot.edit_message_reply_markup(
+                chat_id=call.message.chat.id,
+                message_id=call.message.id,
+                reply_markup=Profile.markup(user)
+            )
 
 
 class Command(BaseCommand):
@@ -34,5 +69,5 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         bot.enable_save_next_step_handlers(delay=2)
         bot.load_next_step_handlers()
-        bot.polling(none_stop=False, interval=0)
-        # bot.infinity_polling()
+        # bot.polling(non_stop=False)
+        bot.infinity_polling()
