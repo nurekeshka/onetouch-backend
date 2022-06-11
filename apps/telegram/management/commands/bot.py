720eb58020857ebb0bcb6e2a33a373f2ba66d4c4
nurekeshka <crystalmaiden666123@gmail.com>
2022-06-12 02:16:57 +0600

    feat: add edit first_name, last_name, age, phone

--- apps/telegram/management/commands/bot.py
+++ apps/telegram/management/commands/bot.py
@@ -1,4 +1,3 @@
-from apps.accounts.verification.serializers import PhoneVerificationSerializer
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from ...models import Telegram
@@ -7,6 +6,7 @@ from ...interface import *
 from ...constants import LOGO_URL
 from telebot import types
 from telebot import TeleBot
+import phonenumbers as pns
 
 
 bot = TeleBot(settings.TELEGRAM_BOT_API_KEY, threaded=False)
@@ -51,39 +51,36 @@ def games(message: types.Message):
 def callback_handler(call: types.CallbackQuery, user: Telegram):
     match call.data:
         case Menu.name:
-            bot.edit_message_text(
-                chat_id=call.message.chat.id,
-                message_id=call.message.id,
-                text=Menu.message(user),
-                parse_mode='html'
-            )
-            bot.edit_message_reply_markup(
+            bot.edit_message_media(
+                media=types.InputMediaPhoto(
+                    media=LOGO_URL,
+                    caption=Menu.message(user),
+                    parse_mode='html'
+                ),
                 chat_id=call.message.chat.id,
                 message_id=call.message.id,
                 reply_markup=Menu.markup(user)
             )
 
         case Profile.name:
-            bot.edit_message_text(
-                chat_id=call.message.chat.id,
-                message_id=call.message.id,
-                text=Profile.message(user),
-                parse_mode='html'
-            )
-            bot.edit_message_reply_markup(
+            bot.edit_message_media(
+                media=types.InputMediaPhoto(
+                    media=LOGO_URL,
+                    caption=Profile.message(user),
+                    parse_mode='html'
+                ),
                 chat_id=call.message.chat.id,
                 message_id=call.message.id,
                 reply_markup=Profile.markup(user)
             )
 
         case Games.name:
-            bot.edit_message_text(
-                chat_id=call.message.chat.id,
-                message_id=call.message.id,
-                text=Games.message(),
-                parse_mode='html'
-            )
-            bot.edit_message_reply_markup(
+            bot.edit_message_media(
+                media=types.InputMediaPhoto(
+                    media=LOGO_URL,
+                    caption=Games.message(),
+                    parse_mode='html'
+                ),
                 chat_id=call.message.chat.id,
                 message_id=call.message.id,
                 reply_markup=Games.markup()
@@ -98,24 +95,54 @@ def callback_handler(call: types.CallbackQuery, user: Telegram):
                 except Game.DoesNotExist:
                     return
                 
-                bot.edit_message_text(
-                    chat_id=call.message.chat.id,
-                    message_id=call.message.id,
-                    text=GameDetailed.message(game),
-                    parse_mode='html'
-                )
-                bot.edit_message_reply_markup(
+                bot.edit_message_media(
+                    media=types.InputMediaPhoto(
+                        media=GameDetailed.media(game),
+                        caption=GameDetailed.message(game),
+                        parse_mode='html'
+                    ),
                     chat_id=call.message.chat.id,
                     message_id=call.message.id,
                     reply_markup=GameDetailed.markup(game)
                 )
-                bot.edit_message_media(
-                    media=GameDetailed.media(game),
-                    chat_id=call.message.chat.id,
-                    message_id=call.message.id
-                )
             elif call.data.startswith('edit'):
-                pass
+                match call.data.split(':')[1]:
+                    case 'first_name':
+                        bot.register_next_step_handler(
+                            message=bot.send_message(
+                                chat_id=call.message.chat.id,
+                                text='Отправьте сообщение со своим именем',
+                                parse_mode='html'
+                            ),
+                            callback=edit_first_name
+                        )
+                    case 'last_name':
+                        bot.register_next_step_handler(
+                            message=bot.send_message(
+                                chat_id=call.message.chat.id,
+                                text='Отправьте сообщение со своей фамилией',
+                                parse_mode='html'
+                            ),
+                            callback=edit_last_name
+                        )
+                    case 'age':
+                        bot.register_next_step_handler(
+                            message=bot.send_message(
+                                chat_id=call.message.chat.id,
+                                text='Отправьте сообщение содержащее ваш возраст в цифрах',
+                                parse_mode='html'
+                            ),
+                            callback=edit_age
+                        )
+                    case 'phone':
+                        bot.register_next_step_handler(
+                            message=bot.send_message(
+                                chat_id=call.message.chat.id,
+                                text='Отправьте сообщение содержащее ваш телефонный номер',
+                                parse_mode='html'
+                            ),
+                            callback=edit_phone
+                        )
             else:
                 bot.send_message(
                     chat_id=call.message.chat.id,
@@ -128,15 +155,52 @@ def callback_handler(call: types.CallbackQuery, user: Telegram):
 def edit_first_name(message: types.Message, user: Telegram):
     user.first_name = message.text
     user.save()
-    bot.edit_message_media(
-        media='blob:https://web.telegram.org/dd6bb034-a24e-43df-ac73-8caaec60cc06',
-        chat_id=message.chat.id,
-        message_id=message.id
-    )
 
+    profile(message)
+
+@telegram_user
+def edit_last_name(message: types.Message, user: Telegram):
+    user.last_name = message.text
+    user.save()
+
+    profile(message)
+
+@telegram_user
+def edit_age(message: types.Message, user: Telegram):
+    if message.text.isdigit():
+        user.age = message.text
+        user.save()
+
+        profile(message)
+    else:
+        bot.register_next_step_handler(
+            message=bot.send_message(
+                chat_id=message.chat.id,
+                text='Возраст должен быть цифрой. Отправьте сообщение еще раз которое включает только цифры'
+            ),
+            callback=edit_age
+        )
+
+@telegram_user
+def edit_phone(message: types.Message, user: Telegram):
+    try:
+        if pns.is_possible_number(pns.parse(message.text)):
+            user.phone = message.text
+            user.save()
+            profile(message)
+        else:
+            raise TypeError()
+    except:
+        bot.register_next_step_handler(
+            message=bot.send_message(
+                chat_id=message.chat.id,
+                text='Телефонный номер не подходит по формату или уже существует пользователь с таким телефонным номером. Отправьте сообщение еще раз в формате:\n+7 *** *** ** **'
+            ),
+            callback=edit_phone
+        )
 
 class Command(BaseCommand):
-    help = 'Telegram bot setup command '
+    help = 'Telegram bot setup command'
 
     def handle(self, *args, **kwargs):
         bot.enable_save_next_step_handlers(delay=2)
