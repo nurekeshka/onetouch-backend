1b32ccb6307311ba06cf6fa413f7a0b0104e1bd8
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-13 16:52:42 +0600

    fix: bigger charfield length for all fields in telegram

--- apps/telegram/models.py
+++ apps/telegram/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Telegram(models.Model):
     id = models.IntegerField(primary_key=True, unique=True, null=False, blank=False, verbose_name='id')
-    username = models.CharField(max_length=50, null=True, blank=True, verbose_name='username')
-    first_name = models.CharField(max_length=124, null=True, blank=True, verbose_name='first name')
-    last_name = models.CharField(max_length=124, null=True, blank=True, verbose_name='last name')
+    username = models.CharField(max_length=255, null=True, blank=True, verbose_name='username')
+    first_name = models.CharField(max_length=255, null=True, blank=True, verbose_name='first name')
+    last_name = models.CharField(max_length=255, null=True, blank=True, verbose_name='last name')
     age = models.IntegerField(null=True, blank=True, verbose_name='age')
-    phone = models.CharField(max_length=70, null=True, blank=True, verbose_name='phone')
+    phone = models.CharField(max_length=255, null=True, blank=True, verbose_name='phone')
 
     class Meta:
         verbose_name = 'телеграм'
