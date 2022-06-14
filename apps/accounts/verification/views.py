b293c605eba95457b8c7d3dca6f21b92426bf967
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-15 04:05:19 +0600

    fix: changed service.py file name to utils.py

--- apps/accounts/verification/views.py
+++ apps/accounts/verification/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from . import service
+from . import utils
 
 
 @api_view(['POST'])
@@ -10,11 +10,11 @@ def start_phone_verification(request):
     if phone is None:
         return Response(data={'error': 'phone is not specified'}, status=400)
     
-    body, status = service.start_new_verification(phone)
+    body, status = utils.start_new_verification(phone)
     return Response(data=body, status=status)
 
 
 @api_view(['PUT'])
 def verificate_phone(request):
-    body, status = service.verify(request.POST)
+    body, status = utils.verify(request.POST)
     return Response(data=body, status=status)
