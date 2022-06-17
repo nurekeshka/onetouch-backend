dbf0eeaa2e2836df77f397832ace4245c150b5ef
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-17 14:19:26 +0600

    feat: add test start phone verification invalid

--- apps/accounts/verification/tests.py
+++ apps/accounts/verification/tests.py
@@ -5,11 +5,27 @@ from django.urls import reverse
 
 
 class VerificationTest(APITestCase):
-    def test_start_phone_verification(self):
+    def test_start_phone_verification_valid(self):
         url = reverse('start-phone-verification')
-        phone_string = '+77003377191'
-        data = { 'phone': phone_string }
+        data = { 'phone': '+77003377191' }
         response = self.client.post(url, data, format='multipart')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(PhoneVerification.objects.count(), 1)
         self.assertTrue(PhoneVerification.objects.get().code.isdigit())
+
+    
+    def test_start_phone_verification_invalid(self):
+        url = reverse('start-phone-verification')
+
+        invalid_phone_numbers = [
+            'helloworld',
+            '22341234212',
+            '1',
+        ]
+
+        for phone in invalid_phone_numbers:
+            data = { 'phone': phone }
+            response = self.client.post(url, data, format='multipart')
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+            self.assertEqual(PhoneVerification.objects.count(), 0)
+
