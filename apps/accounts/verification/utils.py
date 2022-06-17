6536c23bfe1cdbe0637724ed9961696a2d15084f
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-06-17 14:10:59 +0600

    fix: status codes from library instead of int

--- apps/accounts/verification/utils.py
+++ apps/accounts/verification/utils.py
@@ -2,6 +2,7 @@ from .serializers import PhoneVerificationSerializer
 from apps.accounts.constants import confirmed
 from .constants import send_sms_url
 from .models import PhoneVerification
+from rest_framework import status
 from django.conf import settings
 from random import randint
 from requests import get
@@ -16,7 +17,7 @@ def start_new_verification(payload: str) -> tuple:
     if serializer.is_valid():
         verification = serializer.create(serializer.validated_data)
     else:
-        return serializer.errors, 400
+        return serializer.errors, status.HTTP_400_BAD_REQUEST
 
     response = get(
         url=send_sms_url,
@@ -31,7 +32,7 @@ def start_new_verification(payload: str) -> tuple:
 
     # if data.get('code') is 0:
     verification.save()
-    return serializer.data, 201
+    return serializer.data, status.HTTP_201_CREATED
     # else:
     #     verification.delete()
     #     return data, response.status_code
@@ -41,15 +42,15 @@ def verify(payload: dict) -> tuple:
     try:
         verification = PhoneVerification.objects.get(phone=payload.get('phone'))
     except PhoneVerification.DoesNotExist:
-        return {'phone': ['Does not exist in verification table']}, 404
+        return {'phone': ['Does not exist in verification table']}, status.HTTP_404_NOT_FOUND
     
     if verification.code == confirmed:
-        return {'phone': ['Already verified']}, 400
+        return {'phone': ['Already verified']}, status.HTTP_400_BAD_REQUEST
 
     elif verification.code == payload.get('code'):
         verification.code = confirmed
         verification.save()
-        return {'phone': ['Successfully verified']}, 200
+        return {'phone': ['Successfully verified']}, status.HTTP_200_OK
 
     else:
-        return {'phone': ['Phone and code does not match together']}, 404
+        return {'phone': ['Phone and code does not match together']}, status.HTTP_404_NOT_FOUND
