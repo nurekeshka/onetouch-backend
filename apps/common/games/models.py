d07ce42540d0b407945c0ea46fd479a4339e93ef
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-09-14 07:19:37 +0600

    fix: play and team methods

--- apps/common/games/models.py
+++ apps/common/games/models.py
@@ -13,14 +13,17 @@ from datetime import date
 
 
 class Player(models.Model):
-    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, verbose_name='user')
-    telegram = models.ForeignKey(Telegram, on_delete=models.CASCADE, null=True, blank=True, verbose_name='telegram')
-    position = models.CharField(max_length=2, choices=PLAYER_POSITIONS, default=PLAYER_POSITIONS[3][0])
+    user = models.ForeignKey(User, null=True, blank=True,
+                             on_delete=models.CASCADE, verbose_name='user')
+    telegram = models.ForeignKey(
+        Telegram, on_delete=models.CASCADE, null=True, blank=True, verbose_name='telegram')
+    position = models.CharField(
+        max_length=2, choices=PLAYER_POSITIONS, default=PLAYER_POSITIONS[3][0])
 
     class Meta:
         verbose_name = 'игрок'
         verbose_name_plural = 'игроки'
-    
+
     def __str__(self):
         return self.user.get_full_name() if self.user else self.telegram.get_full_name()
 
@@ -32,32 +35,40 @@ class Team(models.Model):
         green = (Emoji.green.value, 'Зеленые')
 
     name = models.CharField(max_length=50, verbose_name='name')
-    players = models.ManyToManyField(Player, blank=True, verbose_name='players')
-    game = models.ForeignKey('Game', on_delete=models.CASCADE, verbose_name='game')
-    emoji = models.CharField(choices=Colors.choices, null=True, max_length=50, verbose_name='emoji')
+    players = models.ManyToManyField(
+        Player, blank=True, verbose_name='players')
+    game = models.ForeignKey(
+        'Game', on_delete=models.CASCADE, verbose_name='game')
+    emoji = models.CharField(choices=Colors.choices,
+                             null=True, max_length=50, verbose_name='emoji')
 
     class Meta:
         verbose_name = 'команда'
         verbose_name_plural = 'команды'
         ordering = ('game',)
-    
+
     def __str__(self):
         return self.name
 
 
 class Game(models.Model):
-    field = models.ForeignKey(Field, on_delete=models.PROTECT, verbose_name='field')
+    field = models.ForeignKey(
+        Field, on_delete=models.PROTECT, verbose_name='field')
     form = models.IntegerField(verbose_name='form')
-    date = models.DateField(auto_now=False, auto_now_add=False, verbose_name='date')
-    start = models.TimeField(auto_now=False, auto_now_add=False, verbose_name='start')
-    end = models.TimeField(auto_now=False, auto_now_add=False, verbose_name='end')
-    payment = models.IntegerField(default=settings.DEFAULT_ENTRY_FEE, verbose_name='payment')
+    date = models.DateField(
+        auto_now=False, auto_now_add=False, verbose_name='date')
+    start = models.TimeField(
+        auto_now=False, auto_now_add=False, verbose_name='start')
+    end = models.TimeField(
+        auto_now=False, auto_now_add=False, verbose_name='end')
+    payment = models.IntegerField(
+        default=settings.DEFAULT_ENTRY_FEE, verbose_name='payment')
 
     class Meta:
         verbose_name = 'игра'
         verbose_name_plural = 'игры'
         ordering = ('date', 'field')
-    
+
     def __str__(self):
         return f'{self.start.strftime("%H:%M")} - {self.end.strftime("%H:%M")}: {self.form - 1}+1, {self.field.address}'
 
@@ -69,25 +80,25 @@ class Game(models.Model):
             available_places -= team.players.count()
 
         return available_places
-    
+
     def is_today(self) -> bool:
         return self.date == date.today()
-    
+
     def detailed(self) -> dict:
         return {
             'адрес': self.field,
             'формат': f'{self.form - 1} + 1',
             'дата': self.date,
             'начало': self.start,
-            'конец': self.end 
+            'конец': self.end
         }
-    
+
     def teams(self) -> list:
         return Team.objects.filter(game=self)
 
 
 @receiver(post_save, sender=Game)
-def create_player(sender, instance, created, **kwargs):
+def create_teams(sender, instance, created, **kwargs):
     if created:
         for name in TEAM_NAMES:
             Team.objects.create(
@@ -96,8 +107,9 @@ def create_player(sender, instance, created, **kwargs):
                 emoji=TEAM_EMOJI[name]
             )
 
+
 @receiver(post_save, sender=Game)
-def save_player(sender, instance, **kwargs):
+def save_teams(sender, instance, **kwargs):
     for name in TEAM_NAMES:
         Team.objects.get(
             game=instance,
@@ -105,10 +117,12 @@ def save_player(sender, instance, **kwargs):
             emoji=TEAM_EMOJI[name]
         ).save()
 
+
 @receiver(post_save, sender=Telegram)
 def create_player(sender, instance, created, **kwargs):
     if instance.is_active():
         Player.objects.get_or_create(telegram=instance)
+
 
 @receiver(post_save, sender=Telegram)
 def save_player(sender, instance, **kwargs):
