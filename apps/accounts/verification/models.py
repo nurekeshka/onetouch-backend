d7e1a3b57c81891b64df92abd0e52f009d0002fa
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-09-14 07:13:36 +0600

    feat: add confirm function

--- apps/accounts/verification/models.py
+++ apps/accounts/verification/models.py
@@ -1,10 +1,19 @@
+from ..constants import CONFIRMED
 from django.db import models
 
 
 class PhoneVerification(models.Model):
-    phone = models.CharField(max_length=15, unique=True, verbose_name='phone number')
-    code = models.CharField(max_length=10, verbose_name='verification')
+    phone = models.CharField(max_length=15, unique=True, verbose_name='телефонный номер')
+    code = models.CharField(max_length=10, verbose_name='код подтверждения')
 
     class Meta:
-        verbose_name = 'телефонный номер'
-        verbose_name_plural = 'телефоные номера'
+        verbose_name = 'верификация'
+        verbose_name_plural = 'верификации'
+    
+    def confirm(self, code: str):
+        if self.code == code:
+            self.code = CONFIRMED
+            self.save()
+            return True
+        else:
+            return False
