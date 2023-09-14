7e523a8ab0a3182686e696bb168364945915315d
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-09-14 07:20:07 +0600

    fix: shift to class based views

--- apps/common/games/urls.py
+++ apps/common/games/urls.py
@@ -1,6 +1,8 @@
+from .constants import GamesRoutes
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get-all/', views.get_all),
+    path('get-all/', views.GamesView.as_view(),
+         name=GamesRoutes.all_games.value),
 ]
