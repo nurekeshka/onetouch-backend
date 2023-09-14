59c2db975bbc7dfcb89e5cb902bab4649a4c9724
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-09-14 07:12:14 +0600

    fix: shift to class based views

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.authtoken import views as drf_token_views
 from django.urls import path, include
+from .constants import AccountsRoutes
 from . import views
 
 urlpatterns = [
     path('verification/', include('apps.accounts.verification.urls')),
-    path('sign-up/', views.create_user),
-    path('sign-in/', drf_token_views.obtain_auth_token),
+    path('sign-up/', views.AccountsView.as_view(), name=AccountsRoutes.sign_up.value),
+    path('sign-in/', drf_token_views.obtain_auth_token, name=AccountsRoutes.sign_in.value),
 ]
