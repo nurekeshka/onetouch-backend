0b19c9ad8e62d1df0632e8cc5016f0bffdf7edc9
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-09-14 07:14:13 +0600

    fix: phone validation

--- apps/accounts/verification/serializers.py
+++ apps/accounts/verification/serializers.py
@@ -1,6 +1,6 @@
-from apps.accounts.models import User
 from rest_framework import serializers
 from .models import PhoneVerification
+from .constants import VerificationErrorMessages
 
 
 class PhoneVerificationSerializer(serializers.ModelSerializer):
@@ -8,15 +8,12 @@ class PhoneVerificationSerializer(serializers.ModelSerializer):
         model = PhoneVerification
         fields = ('phone', 'code')
 
-    def create(self, validated_data):
-        return PhoneVerification.objects.create(**validated_data)
-
     def validate_phone(self, value: str):
         ''' Check if phone answers the format '''
+        if value is None:
+            raise serializers.ValidationError(VerificationErrorMessages.none_phone.value)
+
         if not (~value.find('+') and value[value.find('+') + 1:].isdigit()) or len(value) < 12:
-            raise serializers.ValidationError('Формат телефонного номера не правильный. Он должен содержать: код страны, национальный номер и быть длинной в 12 символов')
-        
-        if User.objects.filter(phone=value).exists():
-            raise serializers.ValidationError('Пользователь с таким телефонным номером уже существует')
+            raise serializers.ValidationError(VerificationErrorMessages.invalid_phone.value)
         
         return value
