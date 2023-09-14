c4025a486511e10293eb69c2c7317564e90042d1
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-09-14 07:10:26 +0600

    feat: add account constants

--- apps/accounts/constants.py
+++ apps/accounts/constants.py
@@ -1,1 +1,19 @@
-confirmed = 'confirmed'
+from enum import Enum
+
+
+class AccountsErrorMessages(Enum):
+    not_exist = 'пользователя с таким телефонным номером не существует'
+    exists = 'пользователь с таким телефонным номером уже существует'
+    invalid_info = 'введенная информация не подходит по формату'
+
+
+class AccountsSuccessMessages(Enum):
+    created = 'аккаунт успешно создан'
+
+
+class AccountsRoutes(Enum):
+    sign_up = 'sign-up'
+    sign_in = 'sign-in'
+
+
+CONFIRMED = 'confirmed'
