f37c9ba3b6f2770d28e483e73f896008ab2c4da3
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-09-14 07:11:38 +0600

    fix: serializer validations

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,29 +1,35 @@
 from rest_framework.authtoken.models import Token
-from .verification.models import PhoneVerification
 from rest_framework import serializers
-from .constants import confirmed
+from .verification.constants import VerificationErrorMessages
+from .verification.models import PhoneVerification
+from .constants import CONFIRMED
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'photo', 'phone', 'birth_date')   
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'photo', 'phone', 'birth_date')
 
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
-        PhoneVerification.objects.get(phone=validated_data.get('phone')).delete()
+        user.set_password(validated_data.get('password'))
+        PhoneVerification.objects.get(
+            phone=validated_data.get('phone')).delete()
 
         return user
 
     def validate_phone(self, value):
         ''' Check if phone exists in verification table '''
         if not PhoneVerification.objects.filter(phone=value).exists():
-            raise serializers.ValidationError('Phone does not exist in verification table')
-        
+            raise serializers.ValidationError(
+                VerificationErrorMessages.not_exist_phone.value)
+
         ''' Check if phone finished verification process '''
-        if PhoneVerification.objects.get(phone=value).code != confirmed:
-            raise serializers.ValidationError('Phone verification process have not finished')
+        if PhoneVerification.objects.get(phone=value).code != CONFIRMED:
+            raise serializers.ValidationError(
+                VerificationErrorMessages.not_verified.value)
 
         return value
 
