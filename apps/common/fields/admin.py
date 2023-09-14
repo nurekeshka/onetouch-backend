0fa19f557c3ec0edcc53ccc49278109a54e084b3
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-09-14 07:15:55 +0600

    fix: pep8 format

--- apps/common/fields/admin.py
+++ apps/common/fields/admin.py
@@ -4,20 +4,24 @@ from . import models
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'link')
-    fields = ('link',)
+    list_display = ('id', 'link', 'field')
+    fields = ('link', 'field')
+
 
 @admin.register(models.Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('raiting', 'description', 'user', 'field')
 
+
 @admin.register(models.Field)
 class FieldAdmin(admin.ModelAdmin):
     list_display = ('address', 'latitude', 'longitude', 'contacts')
-    fields = ('address','latitude', 'longitude', 'photo', 'contacts', 'facilities', 'gis_link')
+    fields = ('address', 'latitude', 'longitude', 'photo',
+              'contacts', 'facilities', 'gis_link')
+
 
 @admin.register(models.Facility)
 class FacilityAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'icons_link')
     fields = ('name', 'icons_link')
-    order_by = ('name')
+    order_by = ('name',)
