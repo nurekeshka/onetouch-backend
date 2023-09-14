c452c83ddcca9e90e6927feae4203c3f85216020
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-09-14 07:17:25 +0600

    fix: serializer fields

--- apps/common/fields/serializers.py
+++ apps/common/fields/serializers.py
@@ -1,14 +1,21 @@
 from rest_framework import serializers
 from .models import Field, Feedback
+from .models import Photo
 
 
 class FieldSerializer(serializers.ModelSerializer):
     class Meta:
         model = Field
-        fields = ('id', 'address', 'latitude', 'longitude', 'photo', 'contacts')
+        fields = ('id', 'address', 'latitude', 'longitude', 'contacts')
 
 
 class FeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feedback
         fields = ('id', 'raiting', 'description', 'user', 'field')
+
+
+class PhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Photo
+        fields = ('id', 'link')
