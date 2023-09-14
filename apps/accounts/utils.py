0377d6bfd21455e024db6609e23eea8013b32da4
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-09-14 07:12:33 +0600

    fix: leave only user token method

--- apps/accounts/utils.py
+++ apps/accounts/utils.py
@@ -1,21 +1,7 @@
 from .serializers import UserSerializer, TokenSerializer
-from rest_framework.authtoken.models import Token  
+from rest_framework.authtoken.models import Token
+from rest_framework import status
 
 
-def create_verified_user(info: dict) -> tuple:
-    user_serializer = UserSerializer(data=info, many=False)
-    
-    if user_serializer.is_valid():
-        user = user_serializer.create(user_serializer.validated_data)
-        user.set_password(info['password'])
-        user.save()
-    else:
-        return user_serializer.errors, 400
-
-    token = Token.objects.get(user=user)
-    token_serializer = TokenSerializer(token, many=False)
-
-    return {
-            'user': user_serializer.data,
-            'token': token_serializer.data
-        }, 201
+def get_user_token(user) -> Token:
+    return Token.objects.get(user=user)
