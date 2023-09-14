acdcf829edee6f4d5424bc78a726c7b15625d82d
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-09-14 07:20:22 +0600

    feat: add game logic methods

--- apps/common/games/utils.py
+++ apps/common/games/utils.py
@@ -1,32 +1,32 @@
-from .serializers import GameSerializer, TeamSerializer
-from .models import Game, Team
+from typing import List
+from ..fields.models import Photo
+from .models import Game
 
 
-# GETTING GAME FOR APPLICATION FEED
-# recieves: date, ordering
+def games_for_day(date: str, ordering: int or None = None):
+    games = Game.objects.filter(date=date).order_by('start')
+    return games[:: ordering]
 
-def get_all_for_day(date: str, ordering: str):
-    games = Game.objects.filter(date=date).order_by('start', )[0 : : 1 if int(ordering) else -1]
 
-    answer = list()
+def serialize_games(games: List[Game]):
+    data = list()
 
     for game in games:
-        answer.append({
-            'address': game.field.address,
-            'field_raiting': game.field.calculate_rate(),
-            'latitude': game.field.latitude,
-            'longitude': game.field.longitude,
-            'players_left': game.players_left(),
-            'photo': game.field.photo.link
-        })
-    
-    return answer, 200
+        data.append(serialize_for_feed(game))
+
+    return data
 
 
-# GETTING DETAILED GAME INFORMATION
-# recieves: id
-
-
-def game_detailed(params):
-    if params.get('id'):
-        return {'id': 'id is not given'}, 400
+def serialize_for_feed(game: Game):
+    return {
+        'id': game.id,
+        'address': game.field.address,
+        'name': f'Игра дома у Нурбека',
+        'start_time': game.start.strftime("%H:%M"),
+        'end_time': game.end.strftime("%H:%M"),
+        'field_raiting': game.field.calculate_rate(),
+        'latitude': game.field.latitude,
+        'longitude': game.field.longitude,
+        'players_left': game.players_left(),
+        'photo': Photo.objects.filter(field=game.field)[0].link
+    }
