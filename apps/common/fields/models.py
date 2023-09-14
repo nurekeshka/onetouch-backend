fafe46f60abdbdfceed12389a5100b3ced4e5a1e
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-09-14 07:16:51 +0600

    feat: add photo model

--- apps/common/fields/models.py
+++ apps/common/fields/models.py
@@ -2,23 +2,14 @@ from apps.accounts.models import User
 from django.db import models
 
 
-class Photo(models.Model):
-    link = models.URLField(null=True, blank=True, unique=True, verbose_name='link')
-
-    class Meta:
-        verbose_name = 'фото'
-        verbose_name_plural = 'фото'
-        ordering = ('link',)
-
-    def __str__(self):
-        return self.link
-
-
 class Feedback(models.Model):
     raiting = models.FloatField(verbose_name='raiting')
-    description = models.TextField(null=True, blank=True, verbose_name='description')
-    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='user')
-    field = models.ForeignKey('Field', on_delete=models.PROTECT, verbose_name='field')
+    description = models.TextField(
+        null=True, blank=True, verbose_name='description')
+    user = models.ForeignKey(
+        User, on_delete=models.PROTECT, verbose_name='user')
+    field = models.ForeignKey(
+        'Field', on_delete=models.PROTECT, verbose_name='field')
 
     class Meta:
         verbose_name = 'отзыв'
@@ -31,31 +22,51 @@ class Feedback(models.Model):
 
 class Field(models.Model):
     address = models.CharField(max_length=254, verbose_name='address')
-    latitude = models.FloatField(null=True, blank=True, verbose_name='latitude')
-    longitude = models.FloatField(null=True, blank=True, verbose_name='longitude')
-    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, null=True, verbose_name='photo')
+    latitude = models.FloatField(
+        null=True, blank=True, verbose_name='latitude')
+    longitude = models.FloatField(
+        null=True, blank=True, verbose_name='longitude')
     contacts = models.TextField(blank=True, verbose_name='contacts')
     facilities = models.ManyToManyField('Facility', verbose_name='facilities')
-    gis_link = models.CharField(max_length=255, null=True, verbose_name='gis_link')
+    gis_link = models.CharField(
+        max_length=255, null=True, verbose_name='gis_link')
 
     class Meta:
         verbose_name = 'поле'
         verbose_name_plural = 'поля'
         ordering = ('address',)
-    
+
     def __str__(self):
         return self.address
-    
+
     def calculate_rate(self):
         feedbacks = Feedback.objects.filter(field=self)
-        
-        summary = 0
         count = len(feedbacks)
 
-        for i in range(count):
-            summary += feedbacks[i].raiting
+        if count is 0:
+            return count
+
+        summary = 0
+
+        for feedback in feedbacks:
+            summary += feedback.raiting
 
         return summary / count
+
+
+class Photo(models.Model):
+    link = models.URLField(null=True, blank=True,
+                           unique=True, verbose_name='link')
+    field = models.ForeignKey(
+        Field, on_delete=models.CASCADE, null=True, verbose_name='field')
+
+    class Meta:
+        verbose_name = 'фото'
+        verbose_name_plural = 'фото'
+        ordering = ('link',)
+
+    def __str__(self):
+        return self.link
 
 
 class Facility(models.Model):
